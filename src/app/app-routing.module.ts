17d6c3e8a3bc05ff5975b0f05d1f287e42155d7c
Ahmed Wahba <>
2020-10-27 23:43:01 +0300

    add vendors from tobi fork branch

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
+import { VendorsComponent } from './vendors/vendors.component';
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: HomeComponent },
+  {path: 'stories', component: VendorsComponent }
 ];
 
 @NgModule({
